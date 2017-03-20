7b8beeb0de16bdafacd882aaa2cfbc186c4627be
konsz <konrad.szwarc@outlook.com>
2017-03-21 00:04:43 +0100

    Dodano audio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let czyStart = 1;	// Informuje że dopiero rozpoczynamy grę.
 let gwarantowane = '0 zł';
 let $rewardLine = $('.rewardLine');
 let $point = $('.point'); // Kropki przy bieżcym etapie (domyślnie opacity: 0)
@@ -6,12 +6,17 @@ let $answer = $('.answer'); // 4 pola z odpowiedziami
 let $circle = $('.circle');
 let pytania = zaladujPytania();
 let obecnePytanie = 1;
+zacznijGre();
 let poprawnaOdpowiedz = zadajPytanie(obecnePytanie);
 
-$('.endGame').click(wycofajSie);
-$circle.eq(0).click(polNaPol);
-$circle.eq(1).click(pytanieDoPublicznosci);
-$circle.eq(2).click(telefonDoPrzyjaciela);
+/** Wykonuje wszystkie operacje niezbędne do rozpoczęcia gry*/
+function zacznijGre() {
+	zaladujKola(0);
+	$('.endGame').click(wycofajSie);
+	$circle.eq(0).click(polNaPol);
+	$circle.eq(1).click(pytanieDoPublicznosci);
+	$circle.eq(2).click(telefonDoPrzyjaciela);
+}
 
 /** Przetwarza pytania w formacie JSON i zwraca utworzona tablicę */
 function zaladujPytania() {
@@ -120,6 +125,16 @@ function zaladujPytania() {
 /** Zadaje pytanie o podanym numerze i zwraca odpowiedź na nie */
 function zadajPytanie(nr_pytania) {
 
+	if(czyStart === 0) {
+		wylaczAudio();
+		wlaczMuzykeWTle();
+	}
+	else {
+		czyStart = 0;
+		setTimeout(wlaczMuzykeWTle, 3000);
+	}
+
+
 	$rewardLine.eq( 12 - nr_pytania ).addClass('rewardActualLine');
 	$point.eq( 12 - nr_pytania ).css('opacity', '1');
 	$answer.find('span').css('opacity', 1);
@@ -138,29 +153,45 @@ function zadajPytanie(nr_pytania) {
 
 /** Zaznacza wybrana odpowiedź i wywołuje funkcje sprawdzajaca */
 function zaznaczOdpowiedz() {
-	if ($(this).hasClass('answerChecked')) {
+	wylaczAudio();
+	if ($(this).hasClass('answerChecked')) { // Potwierdzenie wyboru
+		$('#wybor_start').get(0).pause();
+		$('#wybor_loop').get(0).pause();
+
 		$answer.unbind('click');
 		sprawdzOdpowiedz($(this).attr('id'));
-	} else {
+
+
+	} else {	// Wybór po raz pierwszy
 		$answer.attr('class', 'answer')
 		$(this).addClass('answerChecked');
+
+		$('#wybor_start').get(0).play();
+		window.setTimeout(function(){
+			if($('#dobra_odpowiedz').get(0).paused && $('#bledna_odpowiedz').get(0).paused){
+				$('#wybor_loop').get(0).loop = true;
+				$('#wybor_loop').get(0).play();
+			}
+		}, 2500);
 	}
 }
 
 /** Sprawdza czy przekazana odpowiedź jest prawidłowa i podejmuje kolejne kroki */
 function sprawdzOdpowiedz(odpowiedz) {
+	wylaczAudio();
 	$(`#${poprawnaOdpowiedz}`).removeClass('answerChecked').addClass('answerCorrect');
-	window.setTimeout(function() {
 		if (odpowiedz === poprawnaOdpowiedz) {
 			if (obecnePytanie !== 12) {
-				grajDalej();
+				$('#dobra_odpowiedz').get(0).play();
+				window.setTimeout(grajDalej, 9000);
 			} else {
+				$('#milion').get(0).play();
 				koniecGry(1);
 			}
 		} else {
+			$('#bledna_odpowiedz').get(0).play();
 			koniecGry(-1);
 		}
-	}, 2000);
 }
 
 /** Usuwa klasy aktywności pól, wywołuje kolejne pytanie i ewentualnie zmienia kwotę gwarantowaną */
@@ -178,6 +209,8 @@ function grajDalej() {
 
 /** Wywołuje koniecGry(0) */
 function wycofajSie() {
+	wylaczAudio();
+	$('#wycofanie').get(0).play();
 	koniecGry(0);
 }
 
@@ -201,6 +234,15 @@ function koniecGry(number) {
 		$('.gameArea').css('background', 'url("img/hubert/h (w).jpg") no-repeat center center');
 		$('#question').html(`Udało Ci się! Moje gratulacje, wygrywasz 1 000 000 zł !`);
 	}
+
+	$(document).click(function(){
+		$('body').click(function(){
+			location.reload();
+		});
+		setTimeout(function(){
+			$('body').unbind('click');
+		}, 500);
+	});
 }
 
 /** Aktywacja koła ratunkowego pół na pół */
@@ -208,6 +250,7 @@ function polNaPol() {
 	$(this).unbind('click');
 	$(this).attr('src', 'img/circles/circle1-not.png');
 	$('.circle:nth-child(1):hover').css({'width': '25%', 'cursor': 'default'});
+	$('#50_50').get(0).play();
 
 	let numbers = [0, 1, 2];
 	shuffle(numbers);
@@ -225,37 +268,46 @@ function pytanieDoPublicznosci() {
 	$(this).attr('src', 'img/circles/circle2-not.png');
 	$('.circle:nth-child(2):hover').css({'width': '25%', 'cursor': 'default'});
 
-	let answersTable = [0,0,0,0];
+	$('#publicznosc1').get(0).play();
+	setTimeout(function(){
+		$('#publicznosc2').get(0).play();
+		setTimeout(function(){
+			$('#publicznosc3').get(0).play();
+			setTimeout(function(){
+				let answersTable = [0,0,0,0];
 
-	for (let i = 0; i < 100; i++) {
-		let number = Math.random();
-		let number2 = Math.random();
-		if (number2 > 0.5) { // Publicznośc z grubsza pewna
-			if (number >= 0.5) {answersTable[0]++}
-			else if (number >= 0.2 && number < 0.5) {answersTable[1]++}
-			else if (number >= 0.1 && number < 0.2 ) {answersTable[2]++}
-			else {answersTable[3]++}
-		} else if (number2 < 0.3) { // Publicznosc w sumie to nie wie
-			if (number >= 0.73) {answersTable[0]++}
-			else if (number >= 0.5 && number < 0.73) {answersTable[1]++}
-			else if (number >= 0.25 && number < 0.5 ) {answersTable[2]++}
-			else {answersTable[3]++}
-		} else {	// Wahanie miedzy 2 odpowiedzimi z dominacja poprawnej
-			if (number >= 0.5) {answersTable[0]++}
-			else if (number >= 0.06 && number < 0.5) {answersTable[1]++}
-			else if (number >= 0.03 && number < 0.06 ) {answersTable[2]++}
-			else {answersTable[3]++}
-		}
-	}
+				for (let i = 0; i < 100; i++) {
+					let number = Math.random();
+					let number2 = Math.random();
+					if (number2 > 0.5) { // Publicznośc z grubsza pewna
+						if (number >= 0.5) {answersTable[0]++}
+						else if (number >= 0.2 && number < 0.5) {answersTable[1]++}
+						else if (number >= 0.1 && number < 0.2 ) {answersTable[2]++}
+						else {answersTable[3]++}
+					} else if (number2 < 0.3) { // Publicznosc w sumie to nie wie
+						if (number >= 0.73) {answersTable[0]++}
+						else if (number >= 0.5 && number < 0.73) {answersTable[1]++}
+						else if (number >= 0.25 && number < 0.5 ) {answersTable[2]++}
+						else {answersTable[3]++}
+					} else {	// Wahanie miedzy 2 odpowiedzimi z dominacja poprawnej
+						if (number >= 0.5) {answersTable[0]++}
+						else if (number >= 0.06 && number < 0.5) {answersTable[1]++}
+						else if (number >= 0.03 && number < 0.06 ) {answersTable[2]++}
+						else {answersTable[3]++}
+					}
+				}
 
-	let numbers = [1, 2, 3];
-	shuffle(numbers);
+				let numbers = [1, 2, 3];
+				shuffle(numbers);
 
-	$answer.not(`#${poprawnaOdpowiedz}`).each(function(index){
-			$(this).find('span:last-child').append(` ${answersTable[numbers[index]]}%`);
-	});
+				$answer.not(`#${poprawnaOdpowiedz}`).each(function(index){
+						$(this).find('span:last-child').append(` ${answersTable[numbers[index]]}%`);
+				});
 
-	$answer.filter(`#${poprawnaOdpowiedz}`).find(' span:last-child').append(` ${answersTable[0]}%`);
+				$answer.filter(`#${poprawnaOdpowiedz}`).find(' span:last-child').append(` ${answersTable[0]}%`);
+			}, 300);
+		}, 2000);
+	}, 1000);
 }
 
 /** Aktywacja koła ratunkowego telefon do przyjaciela */
@@ -263,12 +315,57 @@ function telefonDoPrzyjaciela() {
 	$(this).unbind('click');
 	$(this).attr('src', 'img/circles/circle3-not.png');
 	$('.circle:nth-child(3):hover').css({'width': '25%', 'cursor': 'default'});
+
+	$('#telefon1').get(0).play();
+	setTimeout(function(){
+		$('#telefon2').get(0).loop = true;
+		$('#telefon2').get(0).play();
+		$('body').click(function(){
+			$('#telefon2').get(0).pause();
+			if(!!document.getElementById('telefon3')){
+				$('#telefon3').get(0).play();
+				setTimeout(function(){$('#telefon3').remove();}, 1000);
+			};
+		});
+	}, 1000);
 }
 
-/**
- * Shuffles array in place.
- * @param {Array} a items The array containing the items.
- */
+/** Wczytuje po kolei koła ratunkowe */
+function zaladujKola(nr_kola) {
+	$(`#zaladuj_kolo${(nr_kola+1)}`).get(0).play();
+	$circle.eq(nr_kola).fadeIn(300);
+	nr_kola++;
+	if (nr_kola < 3) {
+		window.setTimeout(function(){
+			zaladujKola(nr_kola);
+		}, 1000);
+	}
+}
+
+/** Wyłącza wszystkie odgrywane dźwięki */
+function wylaczAudio() {
+	$('audio[id!=telefon3]').each(function(index){
+		$(this).get(0).pause();
+		$(this).get(0).currentTime = 0;
+	});
+}
+
+/** Uruchamia muzyke w tle */
+function wlaczMuzykeWTle() {
+
+	if($('.tlo').get(0).loop == false) {
+		$('.tlo').each(function(index){
+			$(this).get(0).loop = true;
+		});
+	}
+
+	let withBackground = Math.random();
+	if (withBackground > 0.66) {$('.tlo').get(0).play();}
+	else if (withBackground < 0.33) {$('.tlo').get(1).play();}
+	else {$('.tlo').get(2).play();}
+}
+
+/** Zmienia kolejność elementów w tablicy na losową */
 function shuffle(a) {
     var j, x, i;
     for (i = a.length; i; i--) {
